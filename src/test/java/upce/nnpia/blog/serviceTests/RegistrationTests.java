64677d1df171faa8e2e7678d12d5afaadd0c9a08
barpi <st52947@student.upce.cz>
2021-06-27 13:45:07 +0200

    test fix

--- src/test/java/upce/nnpia/blog/serviceTests/RegistrationTests.java
+++ src/test/java/upce/nnpia/blog/serviceTests/RegistrationTests.java
@@ -7,22 +7,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import upce.nnpia.blog.BlogApplication;
-import upce.nnpia.blog.dao.RoleDao;
 import upce.nnpia.blog.dto.UserDto;
-import upce.nnpia.blog.entity.Role;
-import upce.nnpia.blog.entity.RoleType;
 import upce.nnpia.blog.entity.User;
 import upce.nnpia.blog.service.RegistrationService;
-import upce.nnpia.blog.service.RoleService;
 import upce.nnpia.blog.service.UserService;
-import upce.nnpia.blog.service.impl.RegistrationServiceImpl;
-import upce.nnpia.blog.service.impl.RoleServiceImpl;
 
-@SpringBootTest(classes = BlogApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BlogApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Import({RegistrationService.class, UserService.class})
 public class RegistrationTests {
 
     @Autowired
